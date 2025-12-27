81b3c3f2d2e1c3ecca9c23ec3950843d416665e8
Bouabid Moukhlis <bobmoukhlis@gmail.com>
2025-12-27 05:52:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
 # =========================
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
-MODEL = os.getenv("MODEL", "llama-3.3-70b-versatile").strip()
+MODEL = os.getenv("MODEL")
 
 SYSTEM_PROMPT = """
 Sei ChatAI Bob, un assistente AI avanzato e professionale.
