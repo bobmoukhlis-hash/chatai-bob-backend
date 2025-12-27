aa2dc213733fa556b027dded9c1b340a3b3c7555
Bouabid Moukhlis <bobmoukhlis@gmail.com>
2025-12-27 03:09:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import time
 from typing import Any, Dict, List, Optional, Tuple
 
 import requests
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, Form
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import os, requests
